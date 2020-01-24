6054a13607a8d1f7d90b19e0c42131c0dae44d88
Damian Tabaczynski <dam.tab@wp.pl>
2020-01-24 21:02:32 +0100

    Add wireframe

---
+++ src/main/java/damian/tab/BankReality/BankRealityApplication.java
@@ -0,0 +1,13 @@
+package damian.tab.BankReality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRealityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRealityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/damian/tab/BankReality/BankRealityApplicationTests.java
@@ -0,0 +1,13 @@
+package damian.tab.BankReality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRealityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
